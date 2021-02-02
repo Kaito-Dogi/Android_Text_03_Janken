12fc9f28e03b3cf279a2b6b3e0f93e0b5344cc25
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-03 01:41:32 +0900

    クラス名変更、コードをシンプルに

--- app/src/main/java/app/doggy/janken/MainActivity.kt
+++ app/src/main/java/app/doggy/janken/MainActivity.kt
@@ -3,6 +3,7 @@ package app.doggy.janken
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
@@ -10,24 +11,28 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     //手を用意。
-    val hands: List<String> = listOf("グー", "チョキ", "パー")
+    val hands: Array<String> = arrayOf("グー", "チョキ", "パー")
+
+    //結果の文を用意。
+    val resultTexts: Array<String> = arrayOf("あなたの勝ち！", "CPUの勝ち！", "あいこ！")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //クリックリスナを設定。
-        gooButton.setOnClickListener(OnClickListener())
-        chokiButton.setOnClickListener(OnClickListener())
-        paButton.setOnClickListener(OnClickListener())
+        gooButton.setOnClickListener(JankenListener())
+        chokiButton.setOnClickListener(JankenListener())
+        paButton.setOnClickListener(JankenListener())
 
     }
 
-    private inner class OnClickListener: View.OnClickListener {
+    private inner class JankenListener: View.OnClickListener {
         override fun onClick(view: View) {
 
             //CPUの手を決定。
             var cpuHand: Int = Random.nextInt(3)
+            Log.d("cpuHand", "$cpuHand")
 
             //CPUの手を表示。
             cpuTextView.text = hands[cpuHand]
@@ -37,74 +42,48 @@ class MainActivity : AppCompatActivity() {
                 R.id.gooButton -> {
 
                     //プレイヤーの手をTextViewに表示。
-                    playerTextView.text = "グー"
+                    playerTextView.text = hands[0]
 
                     //勝敗判定。
-                    when(hands[cpuHand]) {
-                        "グー" -> {
-                            resultTextView.text = "あいこ！"
-                        }
-                        "チョキ" -> {
-                            resultTextView.text = "あなたの勝ち！"
-                        }
-                        "パー" -> {
-                            resultTextView.text = "CPUの勝ち！"
-                        }
+                    when(cpuHand) {
+                        0 -> resultTextView.text = resultTexts[2]
+                        1 -> resultTextView.text = resultTexts[0]
+                        2 -> resultTextView.text = resultTexts[1]
                     }
                 }
 
                 R.id.chokiButton -> {
 
                     //プレイヤーの手をTextViewに表示。
-                    playerTextView.text = "チョキ"
+                    playerTextView.text = hands[1]
 
                     //勝敗判定。
-                    when(hands[cpuHand]) {
-                        "グー" -> {
-                            resultTextView.text = "CPUの勝ち！"
-                        }
-                        "チョキ" -> {
-                            resultTextView.text = "あいこ！"
-                        }
-                        "パー" -> {
-                            resultTextView.text = "あなたの勝ち！"
-                        }
+                    when(cpuHand) {
+                        0 -> resultTextView.text = resultTexts[1]
+                        1 -> resultTextView.text = resultTexts[2]
+                        2 -> resultTextView.text = resultTexts[0]
                     }
                 }
 
                 R.id.paButton -> {
 
                     //プレイヤーの手をTextViewに表示。
-                    playerTextView.text = "パー"
+                    playerTextView.text = hands[2]
 
                     //勝敗判定。
-                    when(hands[cpuHand]) {
-                        "グー" -> {
-                            resultTextView.text = "あなたの勝ち！"
-                        }
-                        "チョキ" -> {
-                            resultTextView.text = "CPUの勝ち！"
-                        }
-                        "パー" -> {
-                            resultTextView.text = "あいこ！"
-                        }
+                    when(cpuHand) {
+                        0 -> resultTextView.text = resultTexts[0]
+                        1 -> resultTextView.text = resultTexts[1]
+                        2 -> resultTextView.text = resultTexts[2]
                     }
                 }
             }
 
             //勝敗に応じて、resultTextViewの文字色を変更。
             when(resultTextView.text) {
-                "あなたの勝ち！" -> {
-                    resultTextView.setTextColor(Color.parseColor("#0000dd"))
-                }
-
-                "CPUの勝ち！" -> {
-                    resultTextView.setTextColor(Color.parseColor("#dd0000"))
-                }
-
-                "あいこ！" -> {
-                    resultTextView.setTextColor(Color.parseColor("#00dd00"))
-                }
+                resultTexts[0] -> resultTextView.setTextColor(Color.parseColor("#0000dd"))
+                resultTexts[1] -> resultTextView.setTextColor(Color.parseColor("#dd0000"))
+                resultTexts[2] -> resultTextView.setTextColor(Color.parseColor("#00dd00"))
             }
         }
     }
